bc80c11966eaece12534e11115a604e4685157d9
ridwanfansuri@gmail.com <ridwanfansuri@gmail.com>
2020-05-26 14:34:24 +0700

    add destini chellenge

---
+++ destini-challenge-starting-master/lib/story.dart
@@ -0,0 +1,13 @@
+class Story {
+// A. storyTitle to store the story text.
+// B. choice1 to store the text for choice 1,
+// C. choice2 to store the text for choice 2.
+String storyTitle;
+String choice1;
+String choice2;
+
+ Story({this.storyTitle, this.choice1, this.choice2});
+
+}
+
+
